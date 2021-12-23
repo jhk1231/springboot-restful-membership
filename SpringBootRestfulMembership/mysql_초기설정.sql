83d70f799e85ec9e9bdd95a28437165892d1c090
jhk1231 <80297404+jhk1231@users.noreply.github.com>
2021-12-23 12:13:42 +0900

    Create Project

---
+++ SpringBootRestfulMembership/mysql_초기설정.sql
@@ -0,0 +1,11 @@
+-- DB 생성--
+-- CREATE DATABASE test CHARACTER SET UTF8;
+
+-- 유저 생성--
+-- CREATE USER 'scott'@'%' IDENTIFIED BY 'Tiger_1234';
+
+-- 권한 설정--
+-- GRANT ALL PRIVILEGES ON test.* TO 'scott'@'%'; 
+
+-- 권한 적용--
+-- FLUSH PRIVILEGES;
